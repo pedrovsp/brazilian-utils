98f2b392b062176d193c92c012df02eba7a1b11b
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-10-27 21:41:51 -0300

    test: add test for payment barcode

--- src/utilities/payment-barcode/index.ts
+++ src/utilities/payment-barcode/index.ts
@@ -1,22 +1,28 @@
-import { formatBoleto, formatConvenio, isValidBoleto, isValidConvenio } from 'utilities';
+import { format as formatBoleto, isValid as isValidBoleto } from '../boleto';
+import { format as formatConvenio, isValid as isValidConvenio } from '../convenio';
 import { onlyNumbers } from '../../helpers';
 
 export const CONVENIO_VALUE = 8;
 export enum PaymentType {
   BOLETO,
   CONVENIO,
+  INVALID,
+}
+
+function checkFirstDigit(digits: string): PaymentType {
+  return +digits.charAt(0) === CONVENIO_VALUE ? PaymentType.CONVENIO : PaymentType.BOLETO;
 }
 
 export function identifyPaymentType(barcode: string): PaymentType {
-  return +barcode.charAt(0) === CONVENIO_VALUE ? PaymentType.CONVENIO : PaymentType.BOLETO;
+  const digits = onlyNumbers(barcode);
+  if (digits.length >= 44 && digits.length <= 48) return checkFirstDigit(digits);
+  return PaymentType.INVALID;
 }
 
 export function isValid(barcode: string): boolean {
   if (!barcode || typeof barcode !== 'string') return false;
 
-  const digits = onlyNumbers(barcode);
-
-  if (identifyPaymentType(digits) === PaymentType.CONVENIO) {
+  if (identifyPaymentType(barcode) === PaymentType.BOLETO) {
     return isValidBoleto(barcode);
   } else {
     return isValidConvenio(barcode);
@@ -24,9 +30,7 @@ export function isValid(barcode: string): boolean {
 }
 
 export function format(barcode: string): string {
-  const digits = onlyNumbers(barcode);
-
-  if (identifyPaymentType(digits) === PaymentType.CONVENIO) {
+  if (checkFirstDigit(barcode) === PaymentType.BOLETO) {
     return formatBoleto(barcode);
   } else {
     return formatConvenio(barcode);
